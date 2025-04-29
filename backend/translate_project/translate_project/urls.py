40b47e31ac30f7412a5e928ef22ed766dc98ab52
Ismael Kane <ismael9kane@gmail.com>
2025-04-28 22:00:09 -0700

    new admin route

--- backend/translate_project/translate_project/urls.py
+++ backend/translate_project/translate_project/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('/api/admin/', admin.site.urls),
     path('api/', include('translator.urls')),  # Include the URLs of the 'translator' app
 ]
